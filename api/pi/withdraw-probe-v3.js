2e8727d220a09ddf3cf50dbf090b807c518fb17d
vuacotuongpi88 <mhhieu2221@gmail.com>
2026-04-22 21:34:54 +0700

    Make withdraw probe v3 return full success JSON

--- api/pi/withdraw-probe-v3.js
+++ api/pi/withdraw-probe-v3.js
@@ -1,6 +1,11 @@
 module.exports = async function handler(req, res) {
   return res.status(200).json({
     ok: true,
-    route: "withdraw-probe-v3"
+    route: "withdraw-probe-v3",
+    withdrawId: "probe_withdraw_001",
+    txid: "probe_txid_ok",
+    amount: 1,
+    newBalance: 19,
+    leftToday: 2
   });
 };
